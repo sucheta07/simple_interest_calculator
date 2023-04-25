de3de4c3a83ad555a3f553446987c948a5a05b43
sucheta07 <86112442+sucheta07@users.noreply.github.com>
2023-04-25 15:18:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function compute(){
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var t = document.getElementById("years").value;
+    
+    var s_i = p*r*t/100;
+    var amount = parseInt(p) + parseFloat(s_i);
+    var result = document.getElementById("result");
+
+    var c_year = new Date().getFullYear() + parseInt(years);
+
+    if(p<=0){
+        alert("Please enter a positive number!");
+        Document.getElementById("principal").focus();
+    }
+    else {
+        result.innerHTML = " If you deposit $" + "<mark>" + principal + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of $" + amount + ",\<br\>in the year " + year + "\<br\>";
+    }
+
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval ;
+}
